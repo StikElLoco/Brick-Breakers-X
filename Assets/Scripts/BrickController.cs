1749323cc771c4a1b63e22871bc2ac4cd721e5b5
StikElLoco <stikelloco@gmail.com>
2022-05-05 22:09:23 +0300

    Added Particles
    Added particles on brick destruction

--- Assets/Scripts/BrickController.cs
+++ Assets/Scripts/BrickController.cs
@@ -6,17 +6,38 @@ public class BrickController : MonoBehaviour
 {
     [SerializeField] private int pointValue;
     [SerializeField] private int brickHp;
+    
+    private ParticleSystem[] brickParticle = new ParticleSystem[3];
+    private int particleIndex;
 
     [SerializeField] MainManager mainManager;
 
     private void Awake()
     {
         mainManager = GameObject.Find("MainManager").GetComponent<MainManager>();
+        brickParticle[0] = GameObject.Find("Particle_BrickG").GetComponent<ParticleSystem>();
+        brickParticle[1] = GameObject.Find("Particle_BrickR").GetComponent<ParticleSystem>();
+        brickParticle[2] = GameObject.Find("Particle_BrickB").GetComponent<ParticleSystem>();
     }
 
     private void Start()
     {
         mainManager.maxScore += pointValue;
+        //if green brick
+        if (pointValue == 2)
+        {
+            particleIndex = 1;
+        }
+        //if red brick
+        if (pointValue == 4)
+        {
+            particleIndex = 2;
+        }
+        //if blue brick
+        if (pointValue == 8)
+        {
+            particleIndex = 3;
+        }
     }
 
     private void OnCollisionEnter(Collision other)
@@ -25,6 +46,22 @@ public class BrickController : MonoBehaviour
 
         if (brickHp <= 0)
         {
+            switch(particleIndex)
+            {
+                case 3:
+                    brickParticle[2].transform.position = this.transform.position;
+                    brickParticle[2].Play();
+                    break;
+                case 2:
+                    brickParticle[1].transform.position = this.transform.position;
+                    brickParticle[1].Play();
+                    break;
+                case 1:
+                    brickParticle[0].transform.position = this.transform.position;
+                    brickParticle[0].Play();
+                    break;
+            }
+
             mainManager.PlayAudioClip(2);
             mainManager.score += pointValue;
             Destroy(gameObject, 0.1f);
