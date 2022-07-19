909c50e8ccdb584af3bb1ac7634080de8001fbfe
StikElLoco <stikelloco@gmail.com>
2022-07-19 04:58:03 +0300

    Spawn system
    small progress, commiting because I don't know when I will mess with it again

--- Assets/Scripts/BrickSpawner.cs
+++ Assets/Scripts/BrickSpawner.cs
@@ -6,21 +6,21 @@ public class BrickSpawner : MonoBehaviour
 {
     [SerializeField] GameObject[] brickLines;
     [SerializeField] GameObject[] brickPrefabs;
+    [SerializeField] int spawnPattern = 0;
+
+    public List<Transform> spawnLocations;
+    //What the fuck https://forum.unity.com/threads/list-or-array-of-lists.513443/
+    public List<List<int>> numbers = new List<List<int>>();
 
     // Start is called before the first frame update
     void Start()
     {
         for(int i = 0; i < brickLines.Length; i++)
         {
-            List<Transform> spawnsLocations = GetChildren(brickLines[i].transform);
-
-            foreach(Transform child in spawnsLocations)
-            {
-                Debug.Log(child.position);
-                Instantiate(brickPrefabs[Random.Range(0, 3)], child);
-            }
+            spawnLocations = GetChildren(brickLines[i].transform);
         }
-
+        //TO DO: Make list of lists or array of lists?
+        SpawnBricks(spawnPattern, spawnLocations);
     }
 
     // Update is called once per frame
@@ -41,4 +41,25 @@ public class BrickSpawner : MonoBehaviour
         return children;
     }
 
+    void SpawnBricks(int pattern, List<Transform> locations)
+    {
+        if (pattern == 0)
+        {
+            foreach (Transform child in locations)
+            {
+                Instantiate(brickPrefabs[Random.Range(0, 3)], child);
+            }
+        }
+
+        if (pattern == 1)
+        {
+            foreach (Transform child in locations)
+            {
+                Instantiate(brickPrefabs[0], child);
+            }
+        }
+
+    }
+
 }
+
